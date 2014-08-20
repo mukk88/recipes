0a1b70c498fef44943d05f97ec5d20664349e63f
isabelle soh <isabellesoh@isabelles-mbp.home>
2014-08-20 00:20:04 -0600

    new msg

--- uploadfile.php
+++ uploadfile.php
@@ -14,17 +14,18 @@ if ((($_FILES["image"]["type"] == "image/gif")
   if ($_FILES["image"]["error"] > 0) {
     echo "Return Code: " . $_FILES["image"]["error"] . "<br>";
   } else {
-    echo "Upload: " . $_FILES["image"]["name"] . "<br>";
-    echo "Type: " . $_FILES["image"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["image"]["size"] / 1024) . " kB<br>";
-    echo "Temp file: " . $_FILES["image"]["tmp_name"] . "<br>";
+    // echo "Upload: " . $_FILES["image"]["name"] . "<br>";
+    // echo "Type: " . $_FILES["image"]["type"] . "<br>";
+    // echo "Size: " . ($_FILES["image"]["size"] / 1024) . " kB<br>";
+    // echo "Temp file: " . $_FILES["image"]["tmp_name"] . "<br>";
     if (file_exists("upload/" . $_FILES["image"]["name"])) {
       echo $_FILES["image"]["name"] . " already exists. ";
     } else {
       move_uploaded_file($_FILES["image"]["tmp_name"], "images/" . $_FILES["image"]["name"]);
       move_uploaded_file($_FILES["pdf"]["tmp_name"], "pdf/" . $_FILES["pdf"]["name"]);
-      echo "Stored in: " . "images/" . $_FILES["image"]["name"];
-      echo "Stored in: " . "pdf/" . $_FILES["pdf"]["name"];
+      // echo "Stored in: " . "images/" . $_FILES["image"]["name"] . "<br>";
+      // echo "Stored in: " . "pdf/" . $_FILES["pdf"]["name"];
+      echo "Upload successful! <br> Go to <a href='http://www.woorecipes.azurewebsites.net'>Home</a> to check it out!";
     }
   }
 } else {
