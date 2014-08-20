28e54158c302ae5ec2165be248cc853be322ca30
isabelle soh <isabellesoh@isabelles-mbp.home>
2014-08-19 23:53:13 -0600

    php file

--- uploadfile.php
+++ uploadfile.php
@@ -3,9 +3,10 @@ if ($_FILES["image"]["error"] > 0) {
   echo "Error: " . $_FILES["image"]["error"] . "<br>";
 } else {
   echo "Upload: " . $_FILES["image"]["name"] . "<br>";
+  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
   echo "Type: " . $_FILES["image"]["type"] . "<br>";
   echo "Size: " . ($_FILES["image"]["size"] / 1024) . " kB<br>";
-  echo "Stored in: " . $_FILES["image"]["tmp_name"];
+  echo "Stored in: " . $_FILES["image"]["tmp_name"] . "<br>";
   echo "Upload: " . $_FILES["pdf"]["name"] . "<br>";
   echo "Type: " . $_FILES["pdf"]["type"] . "<br>";
   echo "Size: " . ($_FILES["pdf"]["size"] / 1024) . " kB<br>";
