0707a2ff4a92c627e60c1741e14d97d39ba3a017
mukk88 <markwoo@gmail.com>
2014-09-02 11:42:18 -0700

    no database

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,31 @@ var autoIncrement = require('mongoose-auto-increment');
 var router = express.Router();
 
 //mongo set up
-var mongoOptions = 
-{ 
-	db: {safe: true }, 
-	server: { 
-		auto_reconnect: true, 
-		socketOptions: {keepAlive: 1}  
-	} 
-};
+// var mongoOptions = 
+// { 
+// 	db: {safe: true }, 
+// 	server: { 
+// 		auto_reconnect: true, 
+// 		socketOptions: {keepAlive: 1}  
+// 	} 
+// };
 
-var connectionString = "mongodb://12345:12345@ds052837.mongolab.com:52837/recipes";
-mongoose.connect(connectionString, mongoOptions);
-var connection = mongoose.createConnection(connectionString);
-autoIncrement.initialize(connection);
+// var connectionString = "mongodb://12345:12345@ds052837.mongolab.com:52837/recipes";
+// mongoose.connect(connectionString, mongoOptions);
+// var connection = mongoose.createConnection(connectionString);
+// autoIncrement.initialize(connection);
 
-var recipeSchema = mongoose.Schema({
-	name:String,
-	image:String,
-	pdf:String,
-	cat:String,
-	tags:String
-});
+// var recipeSchema = mongoose.Schema({
+// 	name:String,
+// 	image:String,
+// 	pdf:String,
+// 	cat:String,
+// 	tags:String
+// });
 
-recipeSchema.plugin(autoIncrement.plugin, { model: 'Recipe', startAt: 1 });
+// recipeSchema.plugin(autoIncrement.plugin, { model: 'Recipe', startAt: 1 });
 
-var Recipe = mongoose.model('Recipe', recipeSchema);
+// var Recipe = mongoose.model('Recipe', recipeSchema);
 
 /* GET and POST */
 router.get('/', function(req, res) {
@@ -37,39 +37,39 @@ router.get('/', function(req, res) {
 
 router.get('/recipes', function(req,res){
 
-	var allRecipes;
-	Recipe.find({}, function(err,recipes){
+	// var allRecipes;
+	// Recipe.find({}, function(err,recipes){
 
-	});
+	// });
 
 	res.render('recipes', {recipes: allRecipes});
 });
 
 router.post('/recipes/add', function(req,res){
-	var name = req.body.name;
-	var image = req.body.image;
-	var pdf = req.body.pdf;
-	var cat = req.body.cat;
-	var tags = req.body.tags;
-	Recipe.findOne({name:name}, function(err, recipe){
-		if(!recipe){
-			var recipe = new Recipe();
-			Recipe.nextCount(function(err,count){
-				if(err) return;
-				recipe.name = name;
-				recipe.image = image;
-				recipe.pdf = pdf;
-				recipe.cat = cat;
-				recipe.tags = tags;
-				recipe.save();
-				res.send('1');
-			});
-		}else{
-			//recipe of that title already exists
-			res.send(false);
-		}
-	});
-	// res.send(1);
+	// var name = req.body.name;
+	// var image = req.body.image;
+	// var pdf = req.body.pdf;
+	// var cat = req.body.cat;
+	// var tags = req.body.tags;
+	// Recipe.findOne({name:name}, function(err, recipe){
+	// 	if(!recipe){
+	// 		var recipe = new Recipe();
+	// 		Recipe.nextCount(function(err,count){
+	// 			if(err) return;
+	// 			recipe.name = name;
+	// 			recipe.image = image;
+	// 			recipe.pdf = pdf;
+	// 			recipe.cat = cat;
+	// 			recipe.tags = tags;
+	// 			recipe.save();
+	// 			res.send('1');
+	// 		});
+	// 	}else{
+	// 		//recipe of that title already exists
+	// 		res.send(false);
+	// 	}
+	// });
+	res.send(1);
 });
 
 
