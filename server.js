36eed7ee394bd0a005d3df676a559e85a4e71d39
mukk88 <markwoo@gmail.com>
2014-09-02 23:06:00 -0700

    to server.js

--- server.js
+++ server.js
@@ -23,9 +23,12 @@
 // });
 
 
-var http = require('http')
-var port = process.env.PORT || 3000;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+#!/usr/bin/env node
+var debug = require('debug')('recipes');
+var app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
