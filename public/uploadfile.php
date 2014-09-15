c17715cc8b175a6d4258247d8c87239be99ced66
mukk88 <markwoo@gmail.com>
2014-09-15 13:11:43 -0700

    image by link upload

--- public/uploadfile.php
+++ public/uploadfile.php
@@ -2,6 +2,7 @@
 $allowedExts = array("gif", "jpeg", "jpg", "png");
 $temp = explode(".", $_FILES["image"]["name"]);
 $extension = end($temp);
+$success = False;
 
 if ((($_FILES["image"]["type"] == "image/gif")
 || ($_FILES["image"]["type"] == "image/jpeg")
@@ -22,13 +23,26 @@ if ((($_FILES["image"]["type"] == "image/gif")
       echo $_FILES["image"]["name"] . " already exists. ";
     } else {
       move_uploaded_file($_FILES["image"]["tmp_name"], "images/" . $_FILES["image"]["name"]);
-      move_uploaded_file($_FILES["pdf"]["tmp_name"], "images/pdf/" . $_FILES["pdf"]["name"]);
+      
       // echo "Stored in: " . "images/" . $_FILES["image"]["name"] . "<br>";
       // echo "Stored in: " . "pdf/" . $_FILES["pdf"]["name"];
-      echo "Upload successful! <br> Go to <a href='http://woorecipes.azurewebsites.net'>Home</a> to check it out!";
+      $success=True;
     }
   }
-} else {
+}
+
+if($_FILES["pdf"]["size"] < 5000000){
+  if ($_FILES["pdf"]["error"] > 0) {
+    echo "Return Code: " . $_FILES["pdf"]["error"] . "<br>";
+  }else{
+    move_uploaded_file($_FILES["pdf"]["tmp_name"], "images/pdf/" . $_FILES["pdf"]["name"]);
+    $success = True;
+  }
+}
+
+if($success){
+  echo "Upload successful! <br> Go to <a href='http://woorecipes.azurewebsites.net'>Home</a> to check it out!";
+}else{
   echo "Invalid file";
 }
 ?>
