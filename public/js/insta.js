447d410e2e75794219052f70a1c7ba9aa206a02c
mukk88 <markwoo@gmail.com>
2016-12-05 00:22:27 +0800

    Update insta.js

--- public/js/insta.js
+++ public/js/insta.js
@@ -4,7 +4,7 @@ var feed = new Instafeed({
     // accessToken:'297875328.467ede5.a3dd90d528464ee3a577e2364558a00d',
     get: 'tagged',
 	tagName: 'remarkabelle',
-    clientId: '10ada4796d6c413596c34d30b093020e',
+    clientId: '00651c2612fb4ef9a0d9c08c32dbf178',
     resolution:'standard_resolution',
     template: '<div class="instacontainer"><img class="instaimage" src="{{image}}" /><br><div class="instacaption">{{caption}}</div></div>',
     limit:20
